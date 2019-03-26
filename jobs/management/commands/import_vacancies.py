33c27b2b6030d1107eadee9716c22e842ce40471
Dmitriy <dmitriy.trochshenko@gmail.com>
2019-03-26 13:15:49 +0600

    Download XML file using management command

--- jobs/management/commands/import_vacancies.py
+++ jobs/management/commands/import_vacancies.py
@@ -1,31 +1,81 @@
+import argparse
 import logging
 import sys
+import zipfile
+from datetime import timedelta
 
+import os
+
+import requests
 from django.core.management import BaseCommand
 from django.db.transaction import atomic
+from django.utils.timezone import now
 
 from jobs.models import Profession, Vacancy, Company
 from jobs.mpsv import MPSVParser
+from social_web_page import settings
 
 logger = logging.getLogger(__name__)
 
+
+def tmp_path(*args):
+    return os.path.join(settings.BASE_DIR, 'tmp', *args)
 
 class Command(BaseCommand):
     help = "Import vacancies from MPSV portal XML file"
 
     def add_arguments(self, parser):
-        parser.add_argument('input_file', )
+        parser.add_argument('--input_file', required=False, type=argparse.FileType(), help="a XML file that is used for import")
+        parser.add_argument('--download', required=False, type=str, help="User `latest` word or specific filename to download vacancies file from https://portal.mpsv.cz", )
 
     def handle(self, *args, **options):
-        input_file = options['input_file']
+        downloaded_xml = None
+        if options.get('download'):
+            filename = self.get_filename(options)
+            self.download(filename)
+            downloaded_xml = self.unzip(filename)
+        input_file = options['input_file'] or downloaded_xml
+        logger.info(f'Parsing file {input_file}')
         parser = MPSVParser(input_file)
-
         success = 0
         for vacancy_data in parser.iterate_vacancies():
             self.create_or_update_vacancy(vacancy_data)
             success += 1
 
         sys.stdout.write('Successfully imported %d vacancies\n' % success)
+
+    def get_filename(self, options):
+        if options['download'] == 'latest':
+            yesterday = now() - timedelta(days=1)
+            return yesterday.strftime('vm%Y%m%d_xml.zip')
+        return options['download']
+
+    def download(self, filename):
+        save_as = tmp_path(filename)
+        if os.path.exists(save_as):
+            logger.warning(f'File {save_as} already exists')
+            return
+        url = f'https://portal.mpsv.cz/portalssz/download/getfile.do?filename={filename}&_lang=cs_CZ'
+        logger.info(f'Downloading from {url}')
+        response = requests.get(url, allow_redirects=True)
+        response.raise_for_status()
+
+        logger.info(f'Saving file as: {save_as}')
+        open(save_as, 'wb').write(response.content)
+
+    def unzip(self, filename):
+        xml_filename = filename.rstrip('_xml.zip') + '.xml'
+        extract_dir = tmp_path(f'{filename}_extracted')
+        path = os.path.join(extract_dir, xml_filename)
+        if os.path.exists(path):
+            return path
+
+        with zipfile.ZipFile(tmp_path(filename)) as zip_file:
+            logger.info(f'unzipping {filename} to {extract_dir}')
+            zip_file.extractall(extract_dir, )
+
+        if os.path.exists(path):
+            return path
 
     @atomic
     def create_or_update_vacancy(self, vacancy_data):
@@ -40,9 +90,10 @@ class Command(BaseCommand):
         }
         vacancy, created = Vacancy.objects.get_or_create(mpsv_id=uid, defaults=defaults)
         if created:
-            logger.debug("Created new vacancy: %s" % vacancy)
+            logger.debug(f"Created new vacancy: {vacancy}")
         else:
-            print(uid)
+            # TODO UPDATE Vacancy here
+            pass
         return vacancy
 
     def get_or_create_company(self, vacancy_data):
