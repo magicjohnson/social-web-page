8edd63573d6e91e662701960899d816cde97eec2
Dmitriy <dmitriy.trochshenko@gmail.com>
2019-04-05 22:26:46 +0600

    updated Vacancy parser to support more fields

--- jobs/mpsv.py
+++ jobs/mpsv.py
@@ -9,31 +9,41 @@ class MPSVParser(object):
         self.xml = xml
         self.tree = ElementTree.parse(xml)
         self.root = self.tree.getroot()
+        self.updated_at = datetime.strptime(self.root.attrib.get('aktualizace'), '%Y-%m-%dT%H:%M:%S')
 
     def iterate_vacancies(self):
         for vacancy_element in self.root:
-            yield VacancyParser(vacancy_element).parse()
+            yield VacancyParser(vacancy_element, self.updated_at).parse()
 
 
 class VacancyParser(object):
-    def __init__(self, vacancy_tree):
+    def __init__(self, vacancy_tree, updated_at=None):
         self.vacancy = vacancy_tree
+        self.updated_at = updated_at
         self.namespaces = {'vm': 'http://portal.mpsv.cz/xml/exportvm'}
 
     def parse(self):
         data = {
-            'uid': self.vacancy.attrib.get('uid'),
+            'uid': self.vacancy.attrib['uid'],
+            'total_vacancies': int(self.vacancy.attrib['celkemVm']),
             'address': self.address,
+            'region_codes': self.region_codes,
             'profession': self.profession,
             'employment_period_from': self.employment_period['from'],
+            'is_for_foreign_workers': self.is_for_foreign_workers,
             'is_full_time': self.is_full_time,
+            'updated_at': self.updated_at
         }
         optional_data = {
+            'comments': self.comments,
+            'town': self.town,
             'company': self.company,
             'report_to': self.report_to,
+            'salary_min': self.salary_min,
+            'salary_max': self.salary_max,
             'employment_period_to': self.employment_period.get('to'),
         }
-        data.update({k:v for k, v in optional_data.items() if v})
+        data.update({k: v for k, v in optional_data.items() if v})
         return data
 
     @property
@@ -46,6 +56,19 @@ class VacancyParser(object):
     def address(self):
         el = self.find('PRACOVISTE')
         return el.text
+
+    @property
+    def town(self):
+        el = self.find('PRACOVISTE')
+        return el.attrib.get('obec')
+
+    @property
+    def region_codes(self):
+        el = self.find('PRACOVISTE')
+        if 'okresKod' in el.attrib:
+            return [el.attrib['okresKod']]
+
+        return el.attrib['seznamOkresuKod'].split(',')
 
     @property
     def profession(self):
@@ -84,13 +107,30 @@ class VacancyParser(object):
     @property
     def comments(self):
         el = self.find('POZNAMKA')
-        if el:
+        if hasattr(el, 'text'):
             return el.text
+
+    @property
+    def salary_min(self):
+        el = self.find('MZDA')
+        if el:
+            return el.attrib['min']
+
+    @property
+    def salary_max(self):
+        el = self.find('MZDA')
+        if el:
+            return el.attrib['max'],
 
     @property
     def is_full_time(self):
         el = self.find('PRACPRAVNI_VZTAH')
         return el.attrib['ppvztahPpPlny'] == 'A'
+
+    @property
+    def is_for_foreign_workers(self):
+        el = self.find('VHODNE_PRO')
+        return el.attrib['cizince'] == 'A'
 
     def find(self, tag):
         return self.vacancy.find('vm:%s' % tag, self.namespaces)
--- jobs/tests/test_mpsv.py
+++ jobs/tests/test_mpsv.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import os
 
-from datetime import date
+from datetime import date, datetime
 
 from jobs.mpsv import MPSVParser
 
@@ -26,12 +26,53 @@ class MPSVParserTest(TestCase):
         vacancy = next(parser.iterate_vacancies())
         expected = {
             'uid': '14918890710',
+            'total_vacancies': 5,
+            'region_codes': ['3703'],
+            'town': 'Kuřim',
             'address': 'Prefa Brno a.s. - 01, Blanenská 1190/121, 664 34  Kuřim',
             'profession': {
                 'code': '71140',
                 'name': 'Betonáři, železobetonáři a příbuzní pracovníci',
             },
+            'is_for_foreign_workers': True,
             'employment_period_from': date(2018, 12, 15),
             'is_full_time': True,
+            'updated_at': datetime(2019, 2, 19, 1, 55, 41)
+        }
+        self.assertEqual(vacancy, expected)
+
+    def test_parse_vacancy_with_optional_data__should_return_that_data(self):
+        parser = MPSVParser(get_test_data('vacancy.xml'))
+        vacancy = next(parser.iterate_vacancies())
+        expected = {
+            'uid': '13729580791',
+            'total_vacancies': 1,
+            'region_codes': [
+                '3100', '3201', '3202', '3203', '3204', '3205', '3206', '3207', '3208', '3209', '3210', '3211', '3212', '3301', '3302', '3303',
+                '3304', '3305', '3306', '3307', '3308', '3401', '3402', '3403', '3404', '3405', '3406', '3407', '3408', '3409', '3410', '3501',
+                '3502', '3503', '3504', '3505', '3506', '3507', '3508', '3509', '3510', '3601', '3602', '3603', '3604', '3605', '3606', '3607',
+                '3608', '3609', '3610', '3611', '3701', '3702', '3703', '3704', '3705', '3706', '3707', '3708', '3709', '3710', '3711', '3712',
+                '3713', '3714', '3801', '3802', '3803', '3804', '3805', '3806', '3807', '3808', '3809', '3810', '3811'
+            ],
+            'address': 'Sconto Nábytek, s.r.o. celá ČR, Benešov, Beroun, Blansko,\n'
+            '      Brno-město, Brno-venkov, Bruntál, Břeclav, Česká Lípa, '
+            'České Budějovice, Český Krumlov, Děčín, Domažlice,\n'
+            '      Frýdek-Místek, Havlíčkův Brod, Hlavní město Praha, Hodonín, '
+            'Hradec Králové, Cheb, Chomutov, Chrudim, Jablonec\n'
+            '      nad\n'
+            '      Niso...\n'
+            '    ',
+            'profession': {
+                'code': '3432',
+                'name': 'Aranžéři a příbuzní pracovníci',
+                'addition': 'Dekoratéři obchodního domu pro Česko',
+            },
+            'comments': 'Místo výkonu práce: celá ČR\n'
+             '      Kontakt: Bc. Zdeněk Jásek, e-mail: kariera@sconto.cz\n'
+             '    ',
+            'employment_period_from': date(2017, 12, 20),
+            'is_for_foreign_workers': False,
+            'is_full_time': True,
+            'updated_at': datetime(2019, 3, 25, 1, 53, 16),
         }
         self.assertEqual(vacancy, expected)
