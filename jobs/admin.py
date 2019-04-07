a7e1ed88cee3492e113b046151b321bc078440e3
Dmitriy <dmitriy.trochshenko@gmail.com>
2019-04-07 14:27:30 +0600

    Admin list display modified, added search and filters

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,10 +1,59 @@
+from admin_numeric_filter.admin import SliderNumericFilter, NumericFilterModelAdmin
 from django.contrib import admin
+from django.utils.html import format_html
+from rangefilter.filter import DateRangeFilter
 
 from jobs import models
 
 
+class CustomSliderNumericFilter(SliderNumericFilter):
+    MAX_DECIMALS = 0
+    STEP = 10
+
 @admin.register(models.Vacancy)
-class VacancyAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'mpsv_id', 'profession', 'company', 'created', 'modified')
+class VacancyAdmin(NumericFilterModelAdmin):
+    list_display = (
+        'mpsv_id',
+        'total_vacancies',
+        'profession',
+        'get_contact',
+        'employment_period_to',
+        'get_salary',
+        'is_for_foreign_workers',
+        'mpsv_updated_at',
+        'get_region_codes',
+        'town',
+    )
 
+    search_fields = (
+        'region_codes__code',
+        'profession__code',
+    )
 
+    list_filter = (
+        ('salary_min', CustomSliderNumericFilter),
+        ('salary_max', CustomSliderNumericFilter),
+        ('employment_period_to', DateRangeFilter),
+        'is_for_foreign_workers',
+    )
+
+    def get_region_codes(self, obj):
+        codes = [r.code for r in obj.region_codes.all()[:5]]
+        return '\n'.join(codes) + '...' if len(codes) > 5 else ''
+
+    get_region_codes.short_description = 'Region codes'
+
+    def get_contact(self, obj):
+        if not obj.report_to:
+            return
+        return format_html('{}<br> {}', obj.report_to.email, obj.report_to.phone)
+
+    get_contact.short_description = 'Email, phone'
+
+    def get_salary(self, obj):
+        salary_min_str = '%.0d' % obj.salary_min if obj.salary_min else '...'
+        salary_max_str = '%.0d' % obj.salary_max if obj.salary_max else '...'
+
+        return '%s - %s' % (salary_min_str, salary_max_str)
+
+    get_salary.short_description = 'min/max salary'
